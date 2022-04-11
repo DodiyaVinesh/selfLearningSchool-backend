284132e03abb224ef8fb339cb3df518562db6304
Vinesh Dodiya <dodiyavinesh352@gmail.com>
2022-04-11 13:42:15 +0530

    Added routers

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
-const routes = require("./route");
-const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 require("dotenv").config();
+const routes = require("./route");
 
 const MONGO_URI = process.env.MONGO_URI;
 const PORT = process.env.PORT;
 
-mongoose
-  .connect(MONGO_URI)
-  .then((x) => {
-    console.log("MongoDB connected successfully");
-  })
-  .catch((err) => {
-    console.log("Some Error in connecting mongoDB.\n", err);
-  });
+app.use(cors({ credentials: true, origin: true }));
+app.use(cookieParser());
+app.use(express.json());
+
+// mongoose
+//   .connect(MONGO_URI)
+//   .then((x) => {
+//     console.log("MongoDB connected successfully");
+//   })
+//   .catch((err) => {
+//     console.log("Some Error in connecting mongoDB.\n", err);
+//   });
 
 app.use(routes);
 
